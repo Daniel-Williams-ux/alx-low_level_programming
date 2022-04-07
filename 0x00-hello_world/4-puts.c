266dc961d63cbfd9e397d3c6dc9f000ac1ecc89e
Daniel <williamsoligodaniel@gmail.com>
2022-04-07 21:42:18 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - every point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("/"Programming is kike buiding a multilingual puzzle");
+return (0);
+}
