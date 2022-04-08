bdbb9a98748ef3130f860f90c5ed25bbdedbf188
Daniel <williamsoligodaniel@gmail.com>
2022-04-08 23:18:49 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers 0 to 9 on one line
+ * Return 0
+ */
+int main(void)
+{
+char i;
+
+for (i = '0'; i <= '9'; ++i)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
