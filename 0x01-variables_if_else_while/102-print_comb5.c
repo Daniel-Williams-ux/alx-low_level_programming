c5e7ab639c1b782ca9ae3603a0cca86b0c973d89
Daniel <williamsoligodaniel@gmail.com>
2022-04-09 03:50:40 +0100

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,9 +17,9 @@ putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
 putchar(' ');
 putchar((num2 / 10) + '0');
-putchar((num2 % 10) + '0';
+putchar((num2 % 10) + '0');
 
-if (num1 == 98 && num2 == 00)
+if (num1 == 98 && num2 == 99)
 continue;
 
 putchar(',');
