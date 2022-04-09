b34d0a87c0ff9209dc6ca2bb01ce3aa21e25422f
Daniel <williamsoligodaniel@gmail.com>
2022-04-09 02:16:39 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers 0 to 9 on one line
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; ++i)
+putchar('0' + i);
+
+putchat('\n');
+
+return (0);
+}
