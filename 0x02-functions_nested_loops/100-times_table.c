7f720a221d1daecbaada1b08d1abdc4353e6827f
Daniel <williamsoligodaniel@gmail.com>
2022-04-12 21:28:18 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,9 +39,11 @@ void print_times_table(int n)
 	     if (prod <= 99)
 		     _putchar(' ');
 	     if (prod <= 9)
+	     {
 		     _putchar(' ');
 	             _putchar(k + '0');
 	     } else if (k >= 10 && k < 100)
+	     {
 
 		     if (prod >= 100)
 		     {
@@ -51,6 +53,7 @@ void print_times_table(int n)
 	                     _putchar((k / 10) + '0');
 			     _putchar((k % 10) + '0');
 		} else if (k >= 100)
+		{
 			     _putchar((prod / 100) + '0');
 			     _putchar(((prod / 10)) % 10 + '0');									}
 																	else if (prod <= 99 && prod >= 10)
