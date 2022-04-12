9f1a6bab3f553335e8c5eeba22ee0c57e9dfd4b0
Daniel <williamsoligodaniel@gmail.com>
2022-04-12 22:01:21 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,6 +23,7 @@ void print_times_table(int n)
 	     if (b == 0)
 		     _putchar('0' + rep);
 	     else if (rep < 10)
+	     {
 			     _putchar(' ');
 			     _putchar(' ');
 	                     _putchar('0' + rep);
