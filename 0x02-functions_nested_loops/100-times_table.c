a3a67f55b95af84eecc985d98b49f47c0b4fb6cd
Daniel <williamsoligodaniel@gmail.com>
2022-04-12 21:53:50 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,65 +6,41 @@
  * @n: number of the times table
  * print_times_table - Prints the times table of the input,
  *                      starting with 0.
- * @n: The value of the times table to be printed.
+ * Return: void.
  */
 void print_times_table(int n)
 {
-  int i, j, k;
-  int num, mult, prod;
 
-  if (n >= 0 && n <= 15)
+  int a = 0, rep, b;
+  
+  if (n < 0 || n > 15)
+  return;
+  while (a <= n)
   {
-     for (i = 0; i <= n; i++)
-     for (num = 0; num <= n; num++)
+     for (b = 0; b <= n; b++)
      {
-	     for (j = 0; j <= n; j++)
-	     _putchar('0');
-
-     	     for (mult = 1; mult <= n; mult++)
-	{
-	         k = j * i;
-		 if (j == 0)
-		 {
-			 _putchar(k + '0');
-	     } else if (k < 10 && j != 0)	
-	     {
-		     _putchar(',');
-		     _putchar(' ');
-	     _putchar(',');
-	     _putchar(' ');
-
-	     prod = num * mult;
-
-	     if (prod <= 99)
-		     _putchar(' ');
-	     if (prod <= 9)
-	     {
-		     _putchar(' ');
-	             _putchar(k + '0');
-	     } else if (k >= 10 && k < 100)
-	     {
-
-		     if (prod >= 100)
-		     {
-			     _putchar(',');
+	     rep = a* b;
+	     if (b == 0)
+		     _putchar('0' + rep);
+	     else if (rep < 10)
 			     _putchar(' ');
 			     _putchar(' ');
-	                     _putchar((k / 10) + '0');
-			     _putchar((k % 10) + '0');
-		} else if (k >= 100)
+	                     _putchar('0' + rep);
+		}
+             else if (rep < 100)
 		{
-			     _putchar((prod / 100) + '0');
-			     _putchar(((prod / 10)) % 10 + '0');									}
-																	else if (prod <= 99 && prod >= 10)
-																	{
-                              _putchar(',');
-                              _putchar(' ');													      _putchar((k / 100) + '0');
-			      _putchar(((k / 10) % 10) + '0');
-			      _putchar((k % 10) + '0');
-			      _putchar((prod / 10) + '0');
-																	}
-																	_putchar((prod % 10) + '0');
-																	}
-		_putchar('\n');
+			     _putchar(' ');
+			     _putchar('0' + rep / 10);
+			     _putchar('0' + rep % 10);
+	        } 
+	     else 
+	     {
+                              _putchar('0' + rep / 100);
+                              _putchar('0' + (rep - 100) / 10);
+			      _putchar('0' + rep % 10);
+	     }
+  }
+ _putchar('\n');
+ a++;
 	}
+}
