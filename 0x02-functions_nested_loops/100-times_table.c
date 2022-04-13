553404797dbcebdb0f36bd8dd10919e910192d92
Daniel <williamsoligodaniel@gmail.com>
2022-04-13 06:08:38 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,12 +8,12 @@
 
 void print_times_table(int n)
 {
-
-  int num, mult, prod;
+	
+	int num, mult, prod;
   
-  if (n >= 0 && n <= 15)
-  {
-	  
+        if (n >= 0 && n <= 15)
+        {
+
 	  for (num = 0; num <= n; num++)
 	  {
 
