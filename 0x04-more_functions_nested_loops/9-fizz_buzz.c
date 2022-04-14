9806e4eda1e2500a3f61a96ba74cd2cb8b4b2c5d
Daniel <williamsoligodaniel@gmail.com>
2022-04-14 18:10:03 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,28 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 != 0)
-	        {
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-	        } else if (i == 1)
-		{
-			printf("%d", i);
-		} else
-	        {
-			printf(" %d", i);
-		}
-	}
-	printf("\n");
-
-	return (0);
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+} else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("Buzz");
+} else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+putchar(' ');
+else
+putchar('\n');
+}
+return (0);
 }
