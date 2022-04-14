3f5da21bf3252389aa87d19b152b2e1603c213b5
Daniel <williamsoligodaniel@gmail.com>
2022-04-14 18:14:14 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,10 +16,12 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf("Fizz");
-} else if (i % 3 == 0 && i % 5 == 0)
+} 
+else if (i % 3 != 0 && i % 5 == 0)
 {
 printf("Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+} 
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
