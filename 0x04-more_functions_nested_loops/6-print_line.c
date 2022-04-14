94358b7535b545045bfc56de18cb9311bb3bda9a
Daniel <williamsoligodaniel@gmail.com>
2022-04-14 14:48:20 +0100

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,10 +14,9 @@ void print_line(int n)
 		int i;
 
 		for (i = 1; i <= n; i++)
-																	{
-																		_putchar('_');
-																	}
-																	_putchar('\n');
-																}
+		}
+			_putchar('_');													}
+		_putchar('\n');
+	}
 
 }
