be2748b16c091134fb703dabe9e941af76a125cc
Daniel <williamsoligodaniel@gmail.com>
2022-04-14 14:38:44 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,10 @@ void more_numbers(void)
 	for (c = 0; c < 10; c++)
 	{
 	       for (x = 0; x <= 14; x++)
-	       { 
+	       {
 			if (x > 9)
 			{
-			_putchar((x / 10) + 48);
+			        _putchar((x / 10) + 48);
 			}
 			_putchar((x % 10) + 48);
 		}
