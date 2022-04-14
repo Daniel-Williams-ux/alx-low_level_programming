8ad1cbcf95cee1c30dab679997f8d75487f7a703
Daniel <williamsoligodaniel@gmail.com>
2022-04-14 14:34:55 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,8 @@ void more_numbers(void)
 
 	for (c = 0; c < 10; c++)
 	{
-	      for (x = 0; x <= 14; x++)
-	      { 
+	       for (x = 0; x <= 14; x++)
+	       { 
 			if (x > 9)
 			{
 			_putchar((x / 10) + 48);
