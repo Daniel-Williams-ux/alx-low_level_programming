612da76017d69529a878207d14c039a67bcb169a
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-04-24 07:38:03 +0100

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,7 +12,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  *
  * Return: If r can store the sum - a pointer to the result.
  *         If r cannot store the sum - 0.
- */	
+ */
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
