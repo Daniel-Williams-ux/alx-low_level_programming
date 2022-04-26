53af2652a0b76ebbf5b471bdae3d3c811e771844
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-04-26 15:10:33 +0100

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,3 @@
-/**
- * File: main.h
- * Auth: Daniel Williams
- * Desc: Header file containing prototypes for all functions
- * used in the 0x06-pointers_arrays_strings directory.
- */
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
