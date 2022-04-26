655d20cc1c7e748422615e4063765243971c7114
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-04-26 15:02:49 +0100

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,9 @@
-#ifndef main.h
-#define main.h
-
 /**
  * File: main.h
  * Auth: Daniel Williams
  * Desc: Header file containing prototypes for all functions
  * used in the 0x06-pointers_arrays_strings directory.
  */
-
-#include <stddef.h>
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -20,5 +14,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
