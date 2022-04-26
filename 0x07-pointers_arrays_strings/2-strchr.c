6c8ded50331b9c5bf15776a97df1c0b794887450
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-04-26 16:08:26 +0100

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,20 @@
-/*
- * File: 2-strchr.c
- * Auth: Daniel Williams
- */
-
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to be searched.
- * @c: The character to be located.
- *
- * Return: If c is found - a pointer to the first occurence.
- *         If c is not found - NULL.
+ * _memcpy - input
+ * @dest: input pointer to string destination
+ * @src: input pointer to source string
+ * @n: number of bytes
+ * Return: pointer to destination string
  */
-char *_strchr(char *s, char c)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int index;
+	unsigned int i = 0;
 
-	for (index = 0; s[index] >= '\0'; index++)
+	while (i < n)
 	{
-		if (s[index] == c)
-			return (s + index);
+		*(dest + i) = *(src + i);
+		i++;
 	}
-
-	return ('\0');
+	return (dest);
 }
