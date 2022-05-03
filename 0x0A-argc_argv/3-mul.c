6599dc5c4260d27dff05dde4c3b8c6848b2c0f0b
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-05-03 02:35:44 +0100

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * @argc: Counts the number of parameters that go into main
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
