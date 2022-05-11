a4bb9a92e77a164b3108850f54424ac3607712ca
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-05-11 17:19:47 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,7 @@
  * _strdup - returns a pointer to a newly allocated space in memory, which
  * contains a copy of the string given as a parameter.
  * @str: string to copy
- * 
+ *
  * Return: Pointer
  */
 char *_strdup(char *str)
