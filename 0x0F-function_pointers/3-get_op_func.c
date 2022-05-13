f2b57e461e96400f8f3722144237c83bcd3b2515
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-05-13 22:17:34 +0100

    return (NULL);

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,8 +18,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
