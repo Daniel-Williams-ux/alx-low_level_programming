660777430f7dff5d83f87f61aebaecf4bd4de393
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-05-13 22:35:04 +0100

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -30,12 +30,6 @@ int main(int argc, char *argv[])
 
 	for (index = 0; index < bytes; index++)
 	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (index = 0; index < bytes; index++)
-	{
 		opcode = *(unsigned char *)address;
 		printf("%.2x", opcode);
 
