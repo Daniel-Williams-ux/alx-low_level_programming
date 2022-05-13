e7f701ad6c42c2639c4baaa58e7c24b25c6caa9d
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-05-13 21:43:54 +0100

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,14 +20,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);exit(99);
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
-
 	return (0);
 }
