8a90115fe57d880cdf6dd815dbc5c89e0a2cbeb4
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-05-13 21:35:17 +0100

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,22 +1,17 @@
 #ifndef CALC_H
 #define CALC_H
 
-/*
- * File: 3-calc.h
- * Auth: Daniel Williams
- * Desc: Header file containing all structures and
- *       prototypes used by the 3-main.c program.
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 
-/**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
- */
 typedef struct op
 {
-		char *op;
-			int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
