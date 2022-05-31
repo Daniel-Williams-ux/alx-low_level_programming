8b932d5ed41b682d269a7af749eba56ecf540783
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-05-31 03:23:41 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,14 @@
  * Return: void
  */
 
-
 int _strlen(const char *s)
 {
-	int i = 0;
-	while (s[i])
-		i++;
+int i = 0;
+while (s[i])
+i++;
 
-	return (i);
+return (i);
 }
-
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
@@ -28,21 +26,21 @@ int _strlen(const char *s)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int i, len;
+unsigned int n = 0;
+int i, len;
 
-	if (b == NULL)
-		return (0);
+if (b == NULL)
+return (0);
 
-	len = _strlen(b);
+len = _strlen(b);
 
-	for (i = 0; i != len; i++)
-	{
-		if (b[len - i - 1] == '1')
-			n += 1 << i;
-		else if (b[len - i - 1] != '0')
-			return (0);
-	}
+for (i = 0; i != len; i++)
+{
+if (b[len - i - 1] == '1')
+n += 1 << i;
+else if (b[len - i - 1] != '0')
+return (0);
+}
 
-	return (n);
+return (n);
 }
