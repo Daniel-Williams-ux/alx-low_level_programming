7cefc572e4412ef20b0ba1a6694de448862c9bd0
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2022-06-01 14:43:27 +0100

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -45,12 +45,12 @@ void close_file(int fd)
 }
 
 /**
- *  * main - Copies the contents of a file to another file.
- *   * @argc: The number of arguments supplied to the program.
- *    * @argv: An array of pointers to the arguments.
- *     *
- *      * Return: 0 on success.
- *       *
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
  * Description: If the argument count is incorrect - exit code 97.
  *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
